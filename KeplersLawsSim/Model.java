c2ff3ae54ddb8fe11c806b111a021019f3a98540
samueljohnbarr <samueljohnbarr@gmail.com>
2018-01-31 23:15:49 -0500

    Work on orbital path algorithm and stepper function (untested)

--- KeplersLawsSim/Model.java
+++ KeplersLawsSim/Model.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
  * @version 1/31/18
  */
 public class Model {
+    private static final double EARTH_YEAR = 365.25;
     public ArrayList<Body> bodies;
     
     public Model() {
@@ -19,11 +20,32 @@ public class Model {
     /**
      * Steps all planet coordinates by one day
      * @param forward step (true) / backward step (false)
-     * @param circumference of orbit in pixels
+     * @param radius of orbit in pixels
      */
-    public void step(boolean forward, int circumference) {
+    public void step(boolean forward, int radius) {
+        double angle;
+        Body planet;
         for (int i = 0; i < bodies.size(); i++) {
-            //blah
+            planet = bodies.get(i);
+            angle = angularDistance(radius, planet.getOrbitalPeriod(), 1);
+            planet.setX(planet.get(X) + (Math.cos(angle) * radius));
+            planet.setY(planet.get(Y) + (Math.sin(angle) * radius));
         }
+    }
+
+    /**
+     * Returns the angular distance in radians
+     * @param radius of orbit
+     * @param period orbital period of planet
+     * @param days of orbit
+     */
+    public double angularDistance(double radius, double period, 
+                                  int days) {
+        //Find circumference from radius
+        double circumference = Math.pow((Math.PI * radius), 2);
+        //Divide result by orbital period to find angle
+        double angle = (circumference / (period * EARTH_YEAR)) * days;
+   
+        return Math.toRadians(angle);      
     }
 }
